eb54a8205da156021b24d2b54c2050a2f75f1fb0
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-12 20:35:30 +0200

    Add counter project files

---
+++ app/main.js
@@ -0,0 +1,33 @@
+const screenDisplayNumber = document.querySelector(".number");
+const decrementButton = document.querySelector(".decrement");
+const restartButton = document.querySelector(".restart");
+const incrementButton = document.querySelector(".increment");
+const parrafmin = document.querySelector(".min");
+const parrafmax = document.querySelector(".max");
+screenDisplayNumber.textContent = 0;
+
+restartButton.addEventListener("click", () => {
+  screenDisplayNumber.textContent = 0;
+});
+
+incrementButton.addEventListener("click", () => {
+  if (screenDisplayNumber.textContent <= 9) {
+    screenDisplayNumber.textContent++;
+  } else {
+    parrafmax.classList.remove("hidden");
+    setTimeout(function () {
+      parrafmax.classList.add("hidden");
+    }, 3000);
+  }
+});
+
+decrementButton.addEventListener("click", () => {
+  if (screenDisplayNumber.textContent > 0) {
+    screenDisplayNumber.textContent--;
+  } else {
+    parrafmin.classList.remove("hidden");
+    setTimeout(function () {
+      parrafmin.classList.add("hidden");
+    }, 3000);
+  }
+});
